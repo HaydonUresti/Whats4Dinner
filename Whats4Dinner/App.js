ec16bb0b06c73ac4d2d5299a499af9a398c48fb7
HaydonUresti <97619164+HaydonUresti@users.noreply.github.com>
2023-01-13 20:00:02 -0700

    Created a container for the day of the week.

--- Whats4Dinner/App.js
+++ Whats4Dinner/App.js
@@ -3,12 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
+      <Text style={textStyles.headline}>What's 4 Dinner?</Text>
+      <View
+      style={ days.container }>
+      <Text style={textStyles.dayName}>Monday</Text>
+      </View>
+
+    
     </View>
+   
   );
 }
+
+
+
+const dayContainer = StyleSheet.create({
+  container: {
+    flex: 0.25,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
+
+const days = StyleSheet.create({
+  container:  {
+    width: 80,
+    height: 50,
+    backgroundColor: '#F5EE9E',
+  },
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +47,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
+
+const textStyles = StyleSheet.create({
+  centerText: {
+    textAlign: 'center',
+  },
+  dayName: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 15
+  },
+  headline: {
+    textAlign: 'center',
+    fontWeight: 'bold'
+  }
+  
+})
+
+// the styles variable is kind of acting like a class. And the itmes under it like container are like the methods that can be called on the class.
---
+++ screens/MainScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function MainScreen(props) {
+    return (
+        
+    );
+}
+
+export default MainScreen;
+
+
+
+const styles = StyleSheet.create({
+    
+})
